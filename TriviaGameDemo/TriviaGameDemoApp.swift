2417908e585cf1a0f15f0b2b78aaa740d2f20297
Vladimir Ivakhnenko <c-basso@ya.ru>
2022-02-22 21:02:04 +0300

    Initial Commit

---
+++ TriviaGameDemo/TriviaGameDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameDemoApp.swift
+//  TriviaGameDemo
+//
+//  Created by Vladimir Ivakhnenko on 22.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
